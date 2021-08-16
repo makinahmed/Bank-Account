99d806a4951bc9d7c4aa9768e4fce22f6eb046f2
makinahmed <ahmedmakin555@gmail.com>
2021-08-16 12:13:13 +0600

    little optimized

--- js/banking.js
+++ js/banking.js
@@ -8,22 +8,30 @@ let withdraw = document.getElementById('withdraw')   // innerText & previous Val
 let balance = document.getElementById('balance')
 
 
+// functions 
+function getMoneyFromInputField(inputField) {
+    let currentValueText = inputField.value
+    let currentValueNum = parseFloat(currentValueText);
+    return currentValueNum;
+}
+
+function getInnerTextNumber(elementName) {
+    let innerValueText = elementName.innerText
+    let innerValueNum = parseFloat(innerValueText)
+    return innerValueNum;
+}
+//////////////////////
 // deposit button event
 
 depositBtn.addEventListener('click', function () {
     //updating deposit
     let previousDeposit = deposit.innerText;
     let previousDepositAmount = parseFloat(previousDeposit)
-    let newDeposit = depositField.value
-    let newDepositAmount = parseFloat(newDeposit)
+    let newDepositAmount = getMoneyFromInputField(depositField)
     let currentDepositAmount = previousDepositAmount + newDepositAmount;
-
-    let prevBlanace = balance.innerText
-    let prevBlanaceAmount = parseFloat(prevBlanace)
-
+    let prevBlanaceAmount = getInnerTextNumber(balance);
     let totalBalanceAmount = prevBlanaceAmount + newDepositAmount;
     balance.innerText = totalBalanceAmount;
-
     deposit.innerText = currentDepositAmount
     // clear deposit input field 
     depositField.value = ''
@@ -35,26 +43,14 @@ depositBtn.addEventListener('click', function () {
 
 withdrawBtn.addEventListener('click', function () {
 
-    let prevWithdraw = withdraw.innerText;
-    let prevWithdrawAmount = parseFloat(prevWithdraw)
-
-    let currentWithdraw = withDrawField.value;
-    let currentWithdrawAmount = parseFloat(currentWithdraw);
-
+    let prevWithdrawAmount = getInnerTextNumber(withdraw);
+    let currentWithdrawAmount = getMoneyFromInputField(withDrawField);
     let totalWithdraw = prevWithdrawAmount + currentWithdrawAmount;
-
     withdraw.innerText = totalWithdraw;
-
     //balance
-
-    let currentBalance = balance.innerText;
-
-    let currentBalanceAmount = parseFloat(currentBalance);
-
+    let currentBalanceAmount = getInnerTextNumber(balance);
     let newBalanceAmount = currentBalanceAmount - currentWithdrawAmount;
-
     balance.innerText = newBalanceAmount;
-
     // clear deposit input field 
     withDrawField.value = ''
 })
